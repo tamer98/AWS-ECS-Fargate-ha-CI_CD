471d9273d42145fb382a038e517030f628305860
Tamer Taji <tamer.taji@gmail.com>
2026-03-11 20:05:42 +0200

    Delete iam_policy.tf

--- iam_policy.tf
+++
@@ -1,6 +0,0 @@
-
-resource "aws_iam_role_policy" "iam_policy" {
-  name   = "ECS-execution-role-policy"
-  role   = aws_iam_role.ecs_node_role.id
-  policy = file("${path.module}/iam-policy.json")
-}
